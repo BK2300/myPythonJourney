c545539193221650a7e527a2c5ba80f2cf7182e4
BK2300 <BK2300@yahoo.com>
2025-09-26 12:25:40 +0200

    Panda journey

--- pandas/6. Aggregation.py
+++ pandas/6. Aggregation.py
@@ -1,8 +1,36 @@
 # aggregate function = Reduces a set of values into a single summary value. Used to summarize and analyse data
 #                      Often used with the groupby() function
+#                      More specificly the "mean, max, min, sum" etc.
 
 
 
 import pandas as pd
 
+df = pd.read_csv("pokemonData.csv")
+
+#(Whole dataframe)
+    #You cant find the mean of anything thats not numeric, so youll get a error msg
+# print(df.mean(numeric_only=True)) #But with this argument. We print the mean of all the numeric columns and ignore does thats not numeric
+# print(df.sum(numeric_only=True)) #The sum of all numeric columns
+# print(df.min(numeric_only=True)) #minimum value of all numeric columns
+# print(df.max(numeric_only=True)) #maximum value of all numeric columns
+# print(df.count()) #counting the values of each column
+
+
+#(Single column)
+# print(df["Height"].mean()) #The mean of "height" column
+# print(df["Height"].sum())
+# print(df["Height"].min())
+# print(df["Height"].max())
+# print(df["Height"].count())
+
+
+# What if we "group by" their primary type?
+group = df.groupby("Type1")
+
+print(group["Height"].mean()) #So here we found the average Height of all the pokemons, groupped in their fimilar type. (And the result is that, dragon types are on average taller then the rest of them, with a count of 2,66m)
+print(group["Height"].sum()) #seems like water type has the greatest high
+print(group["Height"].min()) #seems like ground has the smallest member
+print(group["Height"].max()) #rock has the one tallest member
+print(group["Height"].count()) #Water type as a count of 28 pokemons combined
 
---
+++ pandas/7. Data cleaning.py
@@ -0,0 +1,18 @@
+# Data cleaniing = the process of ficing/removing:
+#                  incomplete, incorrect, or irrelevant data.
+#                  ~75% of work done with pandas is data cleaning
+
+import pandas as pd
+
+df = pd.read_csv("pokemonData.cvs")
+
+print(df)
+
+
+
+
+
+
+
+
+
